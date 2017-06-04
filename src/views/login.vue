7ab63f8bfb48e552e449f70349ecb2c76ba4abf6
Tony Goldsmith <tony.goldsmith@uptake.com>
2017-06-04 13:57:45 -0500

    isolated get events function for login

--- src/views/login.vue
+++ src/views/login.vue
@@ -22,21 +22,39 @@ export default {
     return {
       fbSignInParams: {
         scope: 'email,user_likes,user_events',
-        return_scopes: true
+        return_scopes: true,
+        futureEvents: [],
+        pastEvents: []
       }
     }
   },
   methods: {
-    onSignInSuccess (response) {
+    getMyEvents () {
       FB.api(
-        "/me/events",
+        "/me/events?limit=100000&type=not_replied",
         function (response) {
+          var futureEvents = [];
+          var pastEvents = []
           if (response && !response.error) {
-              router.push({ name: 'Events'})
-              console.log(response)
+              response.data.forEach(function(event) {
+                var eventDate = new Date(event.start_time)
+                var today = new Date();
+
+                if (eventDate > today) {
+                  futureEvents.push(event)
+                } else {
+                  pastEvents.push(event)
+                }
+              })
+              // router.push({ name: 'Events'})
+              console.log('future', futureEvents)
+              console.log('past', pastEvents)
           }
         }
       )
+    }
+    onSignInSuccess (response) {
+      getMyEvents();
     },
     onSignInError (error) {
       console.log('OH NOES', error)
